32b8d38b0f2399168b7bd1d96a2f0580a26734d1
Maxym Perebiynis <maksperebiynos983@gmail.com>
2023-07-04 19:14:04 +0300

    serilog added and working

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@ using DapperWebAPI.Context;
 using DapperWebAPI.Contracts;
 using DapperWebAPI.Repository;
 using Microsoft.AspNetCore.Builder;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
@@ -15,7 +17,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseRouting();
